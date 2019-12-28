ad0d06217e18ad04574a70e3552db2e35b838246
litepresence <finitestate@tutamail.com>
2019-12-28 10:03:43 -0500

    Update cyphervault.py

--- cyphervault.py
+++ cyphervault.py
@@ -76,12 +76,8 @@ def it(style, text):
     colored text in terminal
     """
     emphasis = {
-        "red": 91,
         "green": 92,
-        "yellow": 93,
-        "blue": 94,
         "purple": 95,
-        "cyan": 96,
     }
     return f"\033[{emphasis[style]}m{str(text)}\033[0m"
 
